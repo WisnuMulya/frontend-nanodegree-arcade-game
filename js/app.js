35f74d3df8e6730888c2bad03dc2b8994b09f20f
Wisnu Mulya <wisnu.p.mulya@gmail.com>
2015-02-24 10:41:48 +0700

    Change keyup to keydown as event to be listened

--- js/app.js
+++ js/app.js
@@ -102,7 +102,8 @@ var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+    // Changing keyup to keydown to improve control experience
+document.addEventListener('keydown', function(e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
